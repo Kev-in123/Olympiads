9a31258452a7399d09f47f1d1d77dc3dcc87ca06
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-02 12:11:05 -0400

    Update Question1.java

--- java/class 3/Question1.java
+++ java/class 3/Question1.java
@@ -4,13 +4,12 @@ import java.lang.Math;
 public class Question1{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to the power calculator!");
     System.out.print("Enter a number: ");
     double base = input.nextDouble();
     System.out.print("Enter another number: ");
     double exponent = input.nextDouble();
     input.close();
     System.out.println(base + " to the power of " + exponent + " is " + Math.pow(base,exponent));
-    
-
   }
 }
