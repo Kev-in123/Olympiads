8a4db37d6735a054552038548a35f6c280f2c9b6
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-16 15:14:43 -0400

    Update question 3.py

--- python/class 7/question 3.py
+++ python/class 7/question 3.py
@@ -1,8 +1,8 @@
-num_1=float(input('Enter a number: '))
-num_2=1.0
+num_1 = float(input('Enter a number: '))
+num_2 = 1.0
 for i in range(10):
   print(f'{num_2} * {num_1} = {num_1*num_2}')
-  num_2+=1
+  num_2 += 1
 
 
 
@@ -13,6 +13,6 @@ for i in range(10):
 
 
 #alternatively you could do 
-num_1=float(input('Enter a number: '))
-[print(f'{i} * {num_1} = {num_1*i}') for i in range(1,11)]
+num_1 = float(input('Enter a number: '))
+[print(f'{i * num_1 = }') for i in range(1,11)]
   
