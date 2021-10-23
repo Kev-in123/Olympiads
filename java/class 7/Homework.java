86a9706ed9331154cdfbb4c4dbd66313f5a6c7eb
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-23 13:47:11 -0400

    Update Homework.java

--- java/class 7/Homework.java
+++ java/class 7/Homework.java
@@ -3,15 +3,15 @@ public class Homework {
     return radius * radius * Math.PI;
   }
 
-  public static double sphere_area(double radius) {
-    return radius * radius * 4 * Math.PI;
+  public static double sphere_ volume(double radius) {
+    return radius * radius * raduis * 4/3 * Math.PI;
   }
 
   public static void main(String []args) {
     System.out.println("The area of a circle with a radius of 4.8 is "
     + circle_area(4.8));
 
-    System.out.println("The area of a sphere with a radius of 3.2 is "
+    System.out.println("The volume of a sphere with a radius of 3.2 is "
     + sphere_area(3.2));
   }
 }
