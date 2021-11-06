08497302603caaa8f90de4f1853bdd4894517a0d
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-06 13:51:19 -0400

    yeet

--- java/class 9/Question5.java
+++ java/class 9/Question5.java
@@ -2,23 +2,22 @@ public class Question5 {
   public static void main(String[] args) {
     int[] numbers = new int[10];
     for (int i = 0; i < 10; i++) {
-      numbers[i] = i + 1;
+      numbers[i] = Integer.parseInt(System.console().readLine());
     }
-
-    System.out.println("\nReversed numbers are: ");
+    System.out.println("Reversed numbers are: ");
     for (int i = 9; i >= 0; i--) {
       System.out.println(numbers[i]);
     }
     System.out.println("Odd numbers are: ");
-    for (int i = 0; i < 10; i++) {
-      if (numbers[i] % 2 != 0) {
-        System.out.println(numbers[i]);
+    for (int i: numbers) {
+      if (i % 2 != 0) {
+        System.out.println(i);
       }
     }
     System.out.println("Even numbers are: ");
-    for (int i = 0; i < 10; i++) {
-      if (numbers[i] % 2 == 0) {
-        System.out.println(numbers[i]);
+    for (int i: numbers) {
+      if (i % 2 == 0) {
+        System.out.println(i);
       }
     }
   }
