5edd267f7f07b0ce861101ca8ee73e76e0c06427
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-06 13:54:29 -0400

    Update Question4.java

--- java/class 9/Question4.java
+++ java/class 9/Question4.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
-
 public class Question4 {
   public static void main(String[] args) { 
-    Scanner input = new Scanner(System.in);
     int[] numbers = new int[5];
     for(int i = 0; i < numbers.length; i++){
       System.out.print("Enter a number: ");
-      numbers[i] = input.nextInt();
+      numbers[i] = Integer.parseInt(System.console().readLine());
     }   
     smallest(numbers);
-    input.close();
-
   }
   public static void smallest(int[] arr){
     int min = Integer.MAX_VALUE;
-    for (int i: arr){
-      if (i < min){
-        min = i;
-      } 
+    for (int i: arr) {
+      min = Math.min(min, i);
     }
     System.out.println("Min: " + min);
   }
