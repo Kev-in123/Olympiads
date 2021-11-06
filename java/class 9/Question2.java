0d0856f0b5ab25c17bea28f592a7087dcf572bcb
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-06 14:00:09 -0400

    Update Question2.java

--- java/class 9/Question2.java
+++ java/class 9/Question2.java
@@ -1,22 +1,17 @@
-import java.util.Scanner;
-
 public class Question2 {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
     int[] numbers = new int[10];
-    for(int i = 0; i < numbers.length; i++) {
+    for (int i = 0; i < 10; i++) {
       System.out.print("Enter a number: ");
-      numbers[i] = input.nextInt();
+      numbers[i] = Integer.parseInt(System.console().readLine());
     }
     printNums(numbers);
-    int sum = sumArray(numbers);
-    System.out.println("The sum of the 10 numbers is: " + sum);
-    input.close();
+    System.out.println("The sum of the 10 numbers is: " + sumArray(numbers));
   }
   public static int sumArray(int[] arr) {
     int sum = 0;
-    for(int i = 0; i < arr.length; i++) {
-      sum += arr[i];
+    for(int i: arr) {
+      sum += i;
     }
     return sum;
   }
