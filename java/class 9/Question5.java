e9b883d0ed7cc2be154c3814edbc2d8ff5fe8089
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-13 12:24:38 -0500

    Update Question5.java

--- java/class 9/Question5.java
+++ java/class 9/Question5.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Question5 {
   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
     int[] numbers = new int[10];
     for (int i = 0; i < 10; i++) {
-      numbers[i] = Integer.parseInt(System.console().readLine());
+      numbers[i] = s.nextInt();
     }
     System.out.println("Reversed numbers are: ");
     for (int i = 9; i >= 0; i--) {
@@ -20,5 +23,6 @@ public class Question5 {
         System.out.println(i);
       }
     }
+    s.close();
   }
 }
