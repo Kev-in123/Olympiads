7b98083774959646364d8b046ff221b88e5d7048
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-23 15:53:29 -0500

    Update Deck.java

--- java/class 11/Deck.java
+++ java/class 11/Deck.java
@@ -1,11 +1,8 @@
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Deck {
   public static void main(String[] args) {
-    
     Set<String> deck = new LinkedHashSet<String>();
-
     for (int i = 1; i <= 13; ++i) {
       String card;
       switch (i) {
@@ -24,23 +21,18 @@ public class Deck {
       default:
         card = String.valueOf(i);
       }
-
       deck.add("Spade " + card); 
       deck.add("Heart " + card);
       deck.add("Club " + card);
       deck.add("Diamond " + card);
     }
-    
     for (int i = 0; i < 7; ++i) {
       shuffle(deck);
     }
-
     for (String card : deck) {
       System.out.println(card);
     }
-
-	}
-
+  }
   public static void shuffle(Set<String> deck) {
     Set<String> pile1 = new LinkedHashSet<String>();
     Set<String> pile2 = new LinkedHashSet<String>();
@@ -51,11 +43,10 @@ public class Deck {
         pile2.add(card);
       }
     }
-
+    deck = new LinkedHashSet<String>();
     for (String card: pile1) {
-       deck.add(card);
+      deck.add(card);
     }
-
     for (String card: pile2) {
       deck.add(card);
     }
