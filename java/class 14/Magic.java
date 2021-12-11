e2002c4b99bf465f1209bfa0ea083aabde36d806
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-12-11 12:05:36 -0500

    Update Magic.java

--- java/class 14/Magic.java
+++ java/class 14/Magic.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Magic{
-  public static void main(String[] args){
+public class Magic {
+  public static void main(String[] args) {
     Deck d = new Deck();
     for (int i = 0; i < 7; i++){
       d.shuffle();
@@ -11,11 +11,11 @@ public class Magic{
     System.out.println(d.cards.get(card_num).getName());
     Card c = d.cards.get(card_num);
     c.setMarked(true);
-    for (int i = 0; i < 7; i++){
+    for (int i = 0; i < 7; i++) {
       d.shuffle();
     }
     System.out.println("abradacadabra");
-    for (int i = 0; i < d.cards.size(); i++){
+    for (int i = 0; i < d.cards.size(); i++) {
       if (d.cards.get(i).isMarked()){
         System.out.println(d.cards.get(i).getName());
         c.setMarked(false);
